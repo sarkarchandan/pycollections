c12519dbd52d3aac47dbe00d2ed5c02d509e1fe4
Chandan Sarkar <sarkar1986.chandan@gmail.com>
2022-01-12 01:02:24 +0530

    chore: Refactor iterator demo
    Refactored the way, in which the demo of the Iterable, nd the Iterator protocols are presented using the customized make targets.

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
-iter_demo:
-	python iterators/itdemo.py
+iter_for:
+	python iterators/itdemo.py iter_for
+call_next:
+	python iterators/itdemo.py call_next
+level_order:
+	python iterators/itdemo.py level_order
+level_order_iterable:
+	python iterators/itdemo.py level_order_iterable
--- iterators/itdemo.py
+++ iterators/itdemo.py
@@ -1,4 +1,27 @@
-from typing import List, Iterator
+from typing import List, Iterator, Any, Sequence
+from argparse import ArgumentParser, Namespace
+
+
+class LevelOrderIterator:
+
+    _seq: Sequence
+    _idx: int
+
+    def __init__(self, seq: Sequence) -> None:
+        self._seq = seq
+        self._idx = 0
+
+    def __next__(self) -> Any:
+        res: Any
+        if self._idx >= len(self._seq):
+            raise StopIteration
+        else:
+            res = self._seq[self._idx]
+        self._idx += 1
+        return res
+
+    def __iter__(self) -> Iterator:
+        return self
 
 
 if __name__ == '__main__':
@@ -8,15 +31,37 @@ if __name__ == '__main__':
     #           +               -
     #       a       b       c       d
     # [*   +   -   a   b   c   d]
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument('target', help='Designates the kind of demonstration',
+                        type=str)
+    args: Namespace = parser.parse_args()
     iter_exp: List[str] = ['*', '+', '-', 'a', 'b', 'c', 'd']
-    print('Using iterator as iterable with for loop.')
-    list_iterator: Iterator = iter(iter_exp)
-    for char in list_iterator:
-        print(char)
-    print('Demonstrate the next function, and the way it works.')
-    list_iterator: Iterator = iter(iter_exp)
-    try:
-        while True:
-            print(next(list_iterator))
-    except StopIteration:
-        print('StopIteration error raised')
+    if args.target == 'iter_for':
+        print('Using iterator as iterable with for loop.')
+        list_iterator: Iterator = iter(iter_exp)
+        for char in list_iterator:
+            print(char)
+    elif args.target == 'call_next':
+        print('Demonstrate the next function, and the way it works.')
+        list_iterator: Iterator = iter(iter_exp)
+        try:
+            while True:
+                print(next(list_iterator))
+        except StopIteration:
+            print('StopIteration error raised')
+    elif args.target == 'level_order':
+        print('Demonstrate the LevelOrderIterator class')
+        lvl_itr: LevelOrderIterator = LevelOrderIterator(seq=iter_exp)
+        try:
+            while True:
+                print(next(lvl_itr))
+        except StopIteration:
+            print('StopIteration error raised')
+    elif args.target == 'level_order_iterable':
+        print('Demonstrate LevelOrderIterator as an Iterable')
+        lvl_itr: LevelOrderIterator = LevelOrderIterator(seq=iter_exp)
+        joined: str = ''.join(lvl_itr)
+        print(f'Expression joined from the Iterable: {joined}')
+    else:
+        raise ValueError('Unsupported value provided as argument')
+
